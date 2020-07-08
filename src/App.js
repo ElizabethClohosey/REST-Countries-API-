80a0b8106da4ff8dd237f16963771d89e661fdb1
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-07-08 14:40:37 -0400

    search/filter layout and basic style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Actions from "./components/Actions/actions"
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.scss";
-
-
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Actions />
+        {/* <SearchResults /> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Actions/actions.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { GoSearch } from "react-icons/go";
+import "bootstrap/dist/css/bootstrap.css";
+import "./actions.scss";
+
+// const toggleDarkMode = () => {
+//   console.log("button clicked");
+//   document.body.classList.toggle("dark-mode");
+// };
+
+class Actions extends Component {
+  render() {
+    return (
+      <section className="actions">
+        <div className="container">
+          <div className="row justify-content-between">
+            <div className="col-6">
+              <GoSearch class="search-icon"/>
+              <input type="text" placeholder="Search for a Country..."></input>
+            </div>
+            <div className="col-3">
+              <select>
+                <option value="">Filter By Region</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+export default Actions; // Don’t forget to use export default!
