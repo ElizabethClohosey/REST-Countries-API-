5bd2ce9ca16d3f433253252cc01fdf6aa78f92bd
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-07-13 11:33:30 -0400

    made code more semantic.  Added component for search results grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import Actions from "./components/Actions/actions"
+import Actions from "./components/Actions/actions";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.scss";
-
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="container">
-        <Actions />
-        {/* <SearchResults /> */}
-      </div>
+      <main>
+        <div className="container">
+          <Actions />
+          {/* <SearchResults /> */}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Search-results/search-results.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { GoSearch } from "react-icons/go";
+import "bootstrap/dist/css/bootstrap.css";
+import "./actions.scss";
+
+
+class Actions extends Component {
+  render() {
+    return (
+      <section className="actions">
+        <div className="container">
+          <div className="row justify-content-between">
+            <div className="col-6">
+              <GoSearch class="search-icon"/>
+              <input type="text" placeholder="Search for a Country..."></input>
+            </div>
+            <div className="col-3">
+              <select>
+                <option value="">Filter By Region</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+export default Actions; // Don’t forget to use export default!
